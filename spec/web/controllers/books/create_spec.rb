886f1e69a1e21c6ce5004347b36de3062b99551a
yashihei <yashihei@gmail.com>
2019-12-04 15:30:13 +0900

    Securing Our Form With Validations

--- spec/web/controllers/books/create_spec.rb
+++ spec/web/controllers/books/create_spec.rb
@@ -1,21 +1,42 @@
 RSpec.describe Web::Controllers::Books::Create, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[book: {title: 'Confident Ruby', author: 'Avdi Grimm'}] }
   let(:repository) { BookRepository.new }
 
   before do
     repository.clear
   end
 
-  it 'creates a new book' do
-    action.call(params)
-    book = repository.last
-    expect(book.id).to_not be_nil
+  context 'with valid params' do
+    let(:params) { Hash[book: { title: 'Confident Ruby', author: 'Avdi Grimm' }] }
+
+    it 'creates a new book' do
+      action.call(params)
+      book = repository.last
+      expect(book.id).to_not be_nil
+      expect(book.title).to eq(params.dig(:book, :title))
+    end
+
+    it 'redirects the user to the books listning' do
+      response = action.call(params)
+      expect(response[0]).to eq(302)
+      expect(response[1]['Location']).to eq('/books')
+    end
   end
 
-  it 'redirects the user to the books listning' do
-    response = action.call(params)
-    expect(response[0]).to eq(302)
-    expect(response[1]['Location']).to eq('/books')
+  context 'with invalid params' do
+    let(:params) { Hash[book: {}] }
+
+    it 'returns HTTP client error' do
+      response = action.call(params)
+      expect(response[0]).to eq(422)
+    end
+
+    it 'dumps errors in params' do
+      action.call(params)
+      errors = action.params.errors
+
+      expect(errors.dig(:book, :title)).to eq(['is missing'])
+      expect(errors.dig(:book, :author)).to eq(['is missing'])
+    end
   end
 end
